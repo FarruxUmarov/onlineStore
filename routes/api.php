b28acd5727099221f235696c2624536e33fd74c7
Farrukh <adler4313@gmail.com>
2024-10-09 19:09:07 +0500

    chore

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\ProductController;
 use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +12,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::resource('/users', UserController::class);
+Route::resource('/products', ProductController::class);
+Route::resource('/categories', CategoryController::class);

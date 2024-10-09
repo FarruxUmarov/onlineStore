8e2dff24017dc44e7358ccd0769af38f6e5aa9c5
Farrukh <adler4313@gmail.com>
2024-10-09 15:00:14 +0500

    feat: add OrderProduct seeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             ImageSeeder::class,
             CommentSeeder::class,
             OrderSeeder::class,
+            OrderProductSeeder::class,
         ]);
     }
 }

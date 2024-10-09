708c5876613b2a7cfd01b736b72cee3e32a00ae6
Farrukh <adler4313@gmail.com>
2024-10-09 10:54:55 +0500

    chore: add Category seeder and factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,10 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'image' => null,
+            'parent_id' => null,
         ];
     }
 }
